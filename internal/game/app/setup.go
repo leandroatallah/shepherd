80f9e26306f9a06a7f7a56d255c72dce7799936c
Leandro Atallah <leandroatallah@gmail.com>
2026-02-19 11:45:00 +0000

    chore: update load audio assets import

--- internal/game/app/setup.go
+++ internal/game/app/setup.go
@@ -51,7 +51,7 @@ func Setup(assets fs.FS) error {
 	dialogueManager := speech.NewManager(speechBubble, speechStory)
 
 	// Load audio assets
-	loadAudioAssetsFromFS(assets, audioManager)
+	audio.LoadAudioAssetsFromFS(assets, audioManager)
 
 	// Load phases
 	for _, p := range GetPhases() {
@@ -90,35 +90,4 @@ func Setup(assets fs.FS) error {
 	}
 
 	return nil
-}
-
-// loadAudioAssetsFromFS is a helper function to load all audio files from an fs.FS.
-func loadAudioAssetsFromFS(assets fs.FS, am *audio.AudioManager) {
-	dir := "assets/audio"
-	files, err := fs.ReadDir(assets, dir)
-	if err != nil {
-		log.Fatalf("error reading embedded audio dir: %v", err)
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-
-		fileName := file.Name()
-		// Filter for supported audio types
-		if !(strings.HasSuffix(fileName, ".ogg") || strings.HasSuffix(fileName, ".wav") || strings.HasSuffix(fileName, ".mp3")) {
-			continue
-		}
-
-		fullPath := dir + "/" + fileName
-		data, err := fs.ReadFile(assets, fullPath)
-		if err != nil {
-			log.Printf("failed to read embedded file %s: %v", fullPath, err)
-			continue
-		}
-
-		// Use the existing Add method to process and store the player.
-		am.Add(dir+"/"+fileName, data)
-	}
 }
