9ab2752b993633ed67b254318e09b93453cb4953
Leandro Atallah <leandroatallah@gmail.com>
2026-02-19 11:38:50 +0000

    chore(engine): promote flash screen effect

--- internal/engine/render/screenutil/utils.go
+++ internal/engine/render/screenutil/utils.go
@@ -9,14 +9,12 @@ import (
 	"github.com/leandroatallah/firefly/internal/engine/data/config"
 )
 
-// TODO: Move
 func GetCenterOfScreenPosition(width, height int) (int, int) {
 	x := config.Get().ScreenWidth/2 - width/2
 	y := config.Get().ScreenHeight/2 - height/2
 	return x, y
 }
 
-// TODO: Move
 func DrawCenteredText(screen *ebiten.Image, fontText *font.FontText, str string, size float64, c color.Color) {
 	textOp := &text.DrawOptions{
 		LayoutOptions: text.LayoutOptions{
