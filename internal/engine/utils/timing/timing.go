a118bdec446c4f1f786d3d2747b75f213961f21a
Leandro Atallah <leandroatallah@gmail.com>
2026-02-23 12:56:49 +0000

    chore(engine): add to duration method and use dynamic TPS

--- internal/engine/utils/timing/timing.go
+++ internal/engine/utils/timing/timing.go
@@ -1,12 +1,23 @@
 package timing
 
-import "time"
+import (
+	"time"
 
-// TPS is the Ticks Per Second of the game engine.
-// Ebitengine defaults to 60 TPS.
-const TPS = 60
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// TPS returns the Ticks Per Second of the game engine.
+// This delegates to Ebitengine's current TPS value.
+func TPS() int {
+	return ebiten.TPS()
+}
 
 // FromDuration converts a time.Duration to the number of frames (ticks) based on TPS.
 func FromDuration(d time.Duration) int {
-	return int(d.Seconds() * TPS)
+	return int(d.Seconds() * float64(TPS()))
+}
+
+// ToDuration converts a number of frames (ticks) to time.Duration based on TPS.
+func ToDuration(frames int) time.Duration {
+	return time.Duration(float64(frames) / float64(TPS()) * float64(time.Second))
 }
