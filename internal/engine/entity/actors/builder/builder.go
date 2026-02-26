103f33bd8566eac37648687ca1970eeaa9b242da
Leandro Atallah <leandroatallah@gmail.com>
2026-02-26 10:47:40 +0000

    refactor(engine): improve platformer actor builder to reduce repeated code

--- internal/engine/entity/actors/builder/builder.go
+++ internal/engine/entity/actors/builder/builder.go
@@ -3,16 +3,56 @@ package builder
 import (
 	"fmt"
 
+	"github.com/leandroatallah/firefly/internal/engine/app"
 	"github.com/leandroatallah/firefly/internal/engine/contracts/animation"
 	"github.com/leandroatallah/firefly/internal/engine/contracts/body"
+	"github.com/leandroatallah/firefly/internal/engine/data/jsonutil"
 	"github.com/leandroatallah/firefly/internal/engine/data/schemas"
 	"github.com/leandroatallah/firefly/internal/engine/entity/actors"
+	"github.com/leandroatallah/firefly/internal/engine/entity/actors/platformer"
 	bodyphysics "github.com/leandroatallah/firefly/internal/engine/physics/body"
+	physicsmovement "github.com/leandroatallah/firefly/internal/engine/physics/movement"
 )
 
 type collisionRectSetter interface {
 	AddCollisionRect(state actors.ActorStateEnum, rect body.Collidable)
 	RefreshCollisions()
+}
+
+// PreparePlatformer loads sprite and stat data, builds the state map, and initializes a PlatformerCharacter.
+func PreparePlatformer(
+	ctx *app.AppContext,
+	jsonPath string,
+) (*platformer.PlatformerCharacter, schemas.SpriteData, actors.StatData, map[string]animation.SpriteState, error) {
+	spriteData, statData, err := jsonutil.ParseSpriteAndStats[actors.StatData](jsonPath)
+	if err != nil {
+		return nil, schemas.SpriteData{}, actors.StatData{}, nil, err
+	}
+
+	stateMap, err := BuildStateMap(spriteData)
+	if err != nil {
+		return nil, schemas.SpriteData{}, actors.StatData{}, nil, err
+	}
+
+	rect := BodyRectFromSpriteData(spriteData)
+	character := platformer.NewPlatformerCharacter(stateMap, spriteData, rect)
+	if character == nil {
+		return nil, schemas.SpriteData{}, actors.StatData{}, nil, fmt.Errorf("failed to create platformer character")
+	}
+	character.SetAppContext(ctx)
+
+	return character, spriteData, statData, stateMap, nil
+}
+
+// ApplyPlatformerPhysics sets up the movement model and touchable interface for a platformer actor.
+func ApplyPlatformerPhysics(actor actors.ActorEntity, blocker physicsmovement.PlayerMovementBlocker) error {
+	model, err := physicsmovement.NewMovementModel(physicsmovement.Platform, blocker)
+	if err != nil {
+		return err
+	}
+	actor.SetMovementModel(model)
+	actor.GetCharacter().SetTouchable(actor)
+	return nil
 }
 
 func SetCharacterBodies(
